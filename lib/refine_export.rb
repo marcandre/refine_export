b847e31332973e252d097df8e2b93d9d667a6e48
Marc-Andre Lafortune <github@marc-andre.ca>
2021-01-22 11:10:50 -0500

    bundle gem refine_export

---
+++ lib/refine_export.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "refine_export/version"
+
+module RefineExport
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/refine_export_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RefineExport do
+  it "has a version number" do
+    expect(RefineExport::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
